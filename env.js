b3e8b4c84c31c1810374568dbc1876ec96371b87
Misemilla2025 <administracion@misemilla.org>
2026-02-15 22:36:32 -0500

    Add configuration for SendGrid API and email addresses
    Agrega archivo env.js con configuración

---
+++ env.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  SENDGRID_API_KEY: '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',
+  CORREO_EMISOR: 'administracion@misemilla.org',
+  CORREO_PRUEBA: 'haralcan16@gmail.com'
+};
