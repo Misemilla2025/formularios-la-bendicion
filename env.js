bae981e99f8c08361336223b602154039268dce8
Misemilla2025 <administracion@misemilla.org>
2026-02-17 11:36:53 -0500

    Refactor env.js to use environment variables
    Removed sensitive configuration details and updated GitHub token usage.

--- env.js
+++ env.js
@@ -1,5 +1,3 @@
-const CONFIG = {
-  SENDGRID_API_KEY: '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',
-  CORREO_EMISOR: 'administracion@misemilla.org',
-  CORREO_PRUEBA: 'haralcan16@gmail.com'
-};
+const GITHUB_USER = "Misemilla2025";
+const GITHUB_REPO = "formularios-la-bendicion";
+const GITHUB_TOKEN = process.env.GH_TOKEN; // Cambia de GITHUB_TOKEN a GH_TOKEN
